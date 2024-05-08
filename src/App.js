5a53dac2a9403283c24aeb19bfe3b6c89dce7bf2
root <root@ip-172-31-0-250.ap-south-1.compute.internal>
2024-05-08 17:19:21 +0000

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios"
+function App() {
+  const hello= async()=>{
+    const res = await axios.get("http://localhost:5000/hello");
+    alert(res.data); 
+  }
+  return (
+    <div className="App">
+<div>
+  <button onClick={hello}>Click Here!</button>
+</div>
+    </div>
+  );
+}
+
+export default App;
